731fe17a68e25fe2a5678d308293d768e16f6513
Andres Ferragut <andres.ferragut@gmail.com>
2022-07-15 17:08:19 -0300

    Updated tc scripts

--- confs/scripts/enlace2.sh
+++ confs/scripts/enlace2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tc qdisc del dev eth0 root
+#tc qdisc del dev eth0 root
 tc qdisc add dev eth0 root handle 1: tbf rate 10mbit burst 5000 limit 12000
 tc qdisc add dev eth0 parent 1:1 handle 10: netem delay 50ms loss 0.5%
